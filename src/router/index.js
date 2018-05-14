946cc99efc5f1a31a16a91dfe6ac1ac6e60e9be8
chengyu <chengyu@jianlc.com>
2018-05-14 15:31:42 +0800

    12345

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,32 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
 Vue.use(Router);
-
 const router = new Router({
-  mode: 'history',
-  scrollBehavior() {
-    return {y: 0};
-  },
-  routes: [{
-      path: "/"
-  },
-  {
-      path: "/wxIndex",
-      component: function(resolve) {
-          require.ensure(['views/wxIndex/wxIndex'], () => {
-              resolve(require('views/wxIndex/wxIndex'));
-          }, 'wxIndex')
-      },
-  }]
+    mode: 'history',
+    scrollBehavior() {
+        return {
+            y: 0
+        };
+    },
+    routes: [{
+        path: "/"
+    }, 
+    {
+        path: "/wxIndex",
+        component: function(resolve) {
+            require.ensure(['views/wxIndex/wxIndex'], () => {
+                resolve(require('views/wxIndex/wxIndex'));
+            }, 'wxIndex')
+        }
+    }, 
+    {
+        path: "/register",  //注册登录
+        component: function(resolve) {
+            require.ensure(['views/register/register'], () => {
+                resolve(require('views/register/register'));
+            }, 'register')
+        }
+    }, 
+    ]
 });
-
 export default router;
---
+++ src/views/register/register.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="registerBox">
+        123
+    </div>
+</template>
+<script src="./register.js"></script>
+<script>
+
+
+export default {
+  data(){
+    return {}
+  },
+  created(){
+    
+  }
+}
+
+</script>
+<style lang="sass" scoped>
+    @import './register.scss'
+</style>
